ceb071abe8ba9f94b50d301ff3a6538c05161e5a
Robocats-5242 <49006486+Robocats-5242@users.noreply.github.com>
2022-01-21 14:30:18 -0600

    added Pixy2 cam library

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -6,12 +6,16 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import io.github.pseudoresonance.pixy2api.Pixy2;
+import io.github.pseudoresonance.pixy2api.Pixy2.LinkType;
 
 public class Vision extends SubsystemBase {
   /** Creates a new Vision. */
 
   private CameraServer cam1;
   
+  private Pixy2 pixy = Pixy2.createInstance(LinkType.SPI);
+
   public Vision() {
     cam1.getInstance().startAutomaticCapture(0);
   }
