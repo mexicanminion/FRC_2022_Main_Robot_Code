7903c1bcf4792cff569cbfe284b1945c5edaa9c3
Robocats-5242 <49006486+Robocats-5242@users.noreply.github.com>
2022-02-01 11:21:22 -0600

    Added intake and shooter subsystems

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -10,6 +10,7 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Robot;
 import io.github.pseudoresonance.pixy2api.Pixy2;
 import io.github.pseudoresonance.pixy2api.Pixy2CCC;
 import io.github.pseudoresonance.pixy2api.Pixy2.LinkType;
@@ -23,11 +24,17 @@ public class Vision extends SubsystemBase {
   private static Pixy2 pixy;
   public static double x;
   public static double y;
+  public static int width;
+  public static int hight;
+  private static Block mainBlock;
 
   public Vision() {
-    //cam1.getInstance().startAutomaticCapture(0);
+    cam1.startAutomaticCapture(0);
     pixy = Pixy2.createInstance(LinkType.SPI);
     pixy.init();
+	hight = pixy.getFrameHeight();
+	width = pixy.getFrameWidth();
+
     SmartDashboard.putString("PixyCam", "initated");
   }
 
@@ -36,34 +43,50 @@ public class Vision extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
-  public static void getBiggestBlockREDBALL() {
+  private static boolean isAligned(Block block){
 	  
-	int blockCount = pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 4);
-	if (blockCount <= 0) {
-		System.err.println("No block count");
+	int blockHalf = (int)(x + block.getWidth() / 2);
+	if(blockHalf <= width/2 + 50 && blockHalf >= width/2 - 50){
+		return true;
 	}
-	ArrayList<Block> blocks = pixy.getCCC().getBlockCache();
-	Block largestBlock = null;
-	if (blocks == null) {
-		System.err.println("No Blocks");
-	}
-	for (Block block : blocks) {
-		if (block.getSignature() == Constants.BlueBallSignature) {
-			if (largestBlock == null) {
-				largestBlock = block;
-			} else if (block.getWidth() > largestBlock.getWidth()) {
-				largestBlock = block;
+	return false;
+  }
+
+  public static void getBiggestBlockCARGO(int CargoSignature) {
+		 int blockCount = pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 4);
+		/*if (blockCount <= 0) {
+			System.err.println("No block count");
+		}*/
+		ArrayList<Block> blocks = pixy.getCCC().getBlockCache();
+		Block largestBlock = null;
+		/*if (blocks == null) {
+			System.err.println("No Blocks");
+		}*/
+		for (Block block : blocks) {
+			if (block.getSignature() == CargoSignature) {
+				if (largestBlock == null) {
+					largestBlock = block;
+				} else if (block.getWidth() > largestBlock.getWidth()) {
+					largestBlock = block;
+				}
 			}
 		}
+
+		if(largestBlock != null){
+			x = largestBlock.getX();
+			y = largestBlock.getY();
+			mainBlock = largestBlock;
+			SmartDashboard.putString("Cargo visible?", "Yes");
+			SmartDashboard.putNumber("Ball X", largestBlock.getX());
+			SmartDashboard.putNumber("Ball Y", largestBlock.getY());
+			SmartDashboard.putBoolean("Aligned?", isAligned(largestBlock));
+		}else{
+			SmartDashboard.putString("Cargo visible?", "No");
+		}
+		
 	}
 
-	x = largestBlock.getX();
-	y = largestBlock.getY();
-	SmartDashboard.putNumber("Ball X", largestBlock.getX());
-	SmartDashboard.putNumber("Ball Y", largestBlock.getY());
-	}
-
-  public static Block getBiggestBlockBLUEBALL() {
+  /*public static Block getBiggestBlockBLUEBALL() {
 		// Gets the number of "blocks", identified targets, that match signature 1 on the Pixy2,
 		// does not wait for new data if none is available,
 		// and limits the number of returned blocks to 4, for a slight increase in efficiency
@@ -82,6 +105,45 @@ public class Vision extends SubsystemBase {
 			}
 		}
 		return largestBlock;
-	}
+	}*/
 
+	public void alignWithTarget(int CargoSignature){
+		boolean isDone = false;
+		Robot.driveBase.parkingBrake(true);
+		double motorPower;
+		double powerFudge = 0;
+	
+		while (isDone == false) {//instead of isDone, add XBoxB
+		  SmartDashboard.putString("align?", "going");
+		  
+		  getBiggestBlockCARGO(CargoSignature);
+
+		  SmartDashboard.putBoolean("Shooter loop", true);
+		  SmartDashboard.putNumber("vision x", x);
+		  motorPower = x * Constants.HomingModifier/27;
+		  if(Math.abs(motorPower) < 0.1){
+			if(motorPower < 0){
+			  powerFudge = -0.08;
+			}else{
+			  powerFudge = 0.08;
+			}
+			
+		  }
+		  SmartDashboard.putNumber("motor power (vision)", motorPower);
+		  Robot.driveBase.drive(-motorPower-powerFudge, motorPower+powerFudge);
+	
+		  if(isAligned(mainBlock)){//(Robot.vision.x > Constants.VisionErrorAllowed){ 
+			isDone = false;
+		  }else{
+			isDone = true;
+			//Robot.hopper.wackerSpinOn();
+		  }
+	
+		  if(Robot.oi.getControllerButtonState(Constants.XBoxButtonHome)){
+			Robot.driveBase.drive(0, 0);
+			isDone = true;
+		  }
+	
+		}
+	}
 }
